1d48e834c00fb16a541b9ed8e659976178c17d69
Christophe <christophe.briguet@me.com>
2023-02-09 10:12:08 -0800

    added sumnmary/verbose modes, support of both Edit distance and distance-ratio

--- logstats.py
+++ logstats.py
@@ -7,15 +7,19 @@ import pickle
 import argparse
 import tlsh
 from Levenshtein import ratio
+from Levenshtein import distance
 
 parser = argparse.ArgumentParser()
 
 parser.add_argument("-f", "--file", dest="file",help="input file name")
 parser.add_argument("-o", "--output", dest="output",help="output file name")
 parser.add_argument("-m", "--method", dest="method",default="distance",help="method use to evaluate similarity (i.e., distance or tlsh)")
-parser.add_argument("-s", "--skip", dest="skip", default="34",type=int,help="Skip n first charcter from header (35 by default)")
-parser.add_argument("-r", "--ratio", dest="ratio", default="	",type=float,help="Levenshtein ratio (0.9 by default)")
-parser.add_argument("-p", "--print", dest="sample", default="3",type=int,help="Print n first sample record (3 by default)")
+parser.add_argument("-s", "--skip", dest="skip", default="0",type=int,help="Skip n first character from header (0 by default)")
+parser.add_argument("-r", "--ratio", dest="ratio", default="	",type=float,help="'distance' or 'distance-ratio' for Levenshtein and 'distance' for TLSH")
+parser.add_argument("-p", "--print", dest="sample", default="3",type=int,help="Print n first sample record (3 by default), need verbose mode enabled")
+parser.add_argument("-v", "--verbose", dest="verbose", default=False,help="Verbose mode")
+parser.add_argument("-S", "--stats", dest="stats", default=False,help="Statistic mode")
+
 args = parser.parse_args()
 
 
@@ -23,7 +27,8 @@ msgType={}
 msgTypeCounter={}
 msgType2ID = {}
 
-
+verbose= args.verbose
+stats=args.stats
 # Ideally the ratio threshold should be adjusted according to data set	 	
 THR = args.ratio 
 
@@ -52,38 +57,40 @@ while line:
 	for key in msgType:
 		# Remove n first characters (e.g. header, timestamp)
 		tmpline = line [args.skip:]
-		tmpkey = key [args.skip:]				
-		
-		if (args.method == "distance"):
+		tmpkey = key [args.skip:]
+		myresult=0			
+		if (args.method == "distance-ratio"):
 				# Compute the Levenshtein ratio between the two strings
 				myresult = ratio(tmpline,tmpkey)
 
+		if (args.method == "distance"):
+				# Compute the Levenshtein distance between the two strings
+				myresult = distance(tmpline,tmpkey)
 		elif (args.method == "tlsh"):
 				# Compute the diff between two hashes
 				h1 = tlsh.hash(str.encode(tmpline))
 				h2 = tlsh.hash(str.encode(tmpkey))
 				if (h1 != "TNULL" and h2 != "TNULL"):
 					myresult = tlsh.diff(h1, h2)
+					#if verbose: print (line)
+					#if verbose: print (h1 + "    "+ h2 + " -> " + str(myresult))
+					#if verbose: print ("\n\n\n")
 
-					if myresult > 0:
-						myresult = 1/myresult
-					
-					else:
-						myresult=1
-					
-					#print (line)
-					#print (h1 + "    "+ h2 + " -> " + str(myresult))
-					#print ("\n\n\n")
-
-		else:
-			myresult = 0
-
-		if (myresult > THR):
+		if (myresult > THR and args.method == "distance-ratio"):
 		# Current line is very similar with one from the list
 			save_key = key
 			save_result = myresult
 			flag = 1
 			break
+
+		if (myresult <= THR and (args.method == "tlsh" or args.method == "distance")):
+		# Current line is very similar with one from the list
+			#print (tmpline,tmpkey,myresult)
+			save_key = key
+			save_result = myresult
+			flag = 1
+			break
+
 	# Current line is similar with a line from msgType dictionary
 	if (flag == 1):	
 	 	# the sample dictionary is not full
@@ -111,21 +118,23 @@ while line:
 		msgTypeCounter[line] = 1
 		msgType2ID[line] = msgType_id
 		msgType_id += 1
-		print ("New type of message discovered line #%s:" %counter)
-		print (line)
-		print ('\n')
+		#print ("New type of message discovered line #%s:" %counter)
+		#print (line)
+		#print ('\n')
 				
 	line = f.readline()
 	if (counter%100 == 0):
-		print ("%s lines analyzed..." %counter)
+		pass
+		if verbose: print ("%s lines analyzed..." %counter)
 	counter += 1
 
 f.close()
 
-print ("%d lines analyzed" %counter)
-print ("%d message types discovered" %len(msgType))
+if verbose: print ("%d lines analyzed" %counter)
+if verbose: print ("%d message types discovered" %len(msgType))
+if stats: print (args.method, THR, len(msgType))
 
-input('Type Enter to display discovered types...')
+if verbose: input('Type Enter to display discovered types...')
 
 outputfile=open(args.output,"w")
 sortedList=[]
@@ -137,27 +146,27 @@ for key1 in sorted (iter(msgTypeCounter.items()), reverse=True, key=lambda kv: k
 	msgID = msgType2ID[key1[0]]
 	percentageOfOccurence= (numberOfOccurence / float(counter))*100
 	stringPercentageOfOccurence="%.2f" % percentageOfOccurence
-	print ("Message UID = %s" % (msgID))
+	if verbose: print ("Message UID = %s" % (msgID))
 	outputfile.write("Message UID = %s" % (msgID))
-	print ("Number of similar message = %s (%s%%)\n" % (numberOfOccurence-1,stringPercentageOfOccurence))
+	if verbose: print ("Number of similar message = %s (%s%%)\n" % (numberOfOccurence-1,stringPercentageOfOccurence))
 	outputfile.write("\nNumber of similar message = %s (%s%%)\n" % (numberOfOccurence-1,stringPercentageOfOccurence))
-	print ("Message type:")
+	if verbose: print ("Message type:")
 	outputfile.write("Message type:\n")
-	print ("----------------\n")
+	if verbose: print ("----------------\n")
 	outputfile.write("----------------\n")
-	print (key1[0])
+	if verbose: print (key1[0])
 	outputfile.write(key1[0])
 	if numberOfOccurence >1:
-		print ("\n\nOther sample of similar messages:")
+		if verbose: print ("\n\nOther sample of similar messages:")
 		outputfile.write("\n\nOther sample of similar messages:\n")
-		print ("-----------------------------\n")
+		if verbose: print ("-----------------------------\n")
 		outputfile.write("-----------------------------\n")
 		for key2 in sorted(iter(msgType[key1[0]].items()), key=lambda kv: kv[1]):
-    	 		#print msgType[key1[0]][key2[0]]
-    	 		print (key2[0])
+    	 		if verbose: print (msgType[key1[0]][key2[0]])
+    	 		if verbose: print (key2[0])
     	 		outputfile.write(key2[0]+'\n')
         	
-	print ("______________________________________________________________________________________________________________________________________\n")
+	if verbose: print ("______________________________________________________________________________________________________________________________________\n")
 	outputfile.write("______________________________________________________________________________________________________________________________________\n\n")
 outputfile.close
 
